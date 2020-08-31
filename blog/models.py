91ebc1c67817606fa49c8ee429ce7cbbb2aca1eb
Your Name <you@example.com>
2020-08-31 22:58:23 +0200

    Add feature to comment on comment post

--- blog/models.py
+++ blog/models.py
@@ -11,12 +11,22 @@ class Post(models.Model):
     published_date = models.DateTimeField(blank=True, null=True)
 
 
-
     def publish(self):
         self.published_date = timezone.now()
         self.save()
 
 
-
     def __str__(self):
         return self.title
+
+
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.text
