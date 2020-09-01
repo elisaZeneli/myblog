7ea06dd76ea850f35dd4eb656d73043a359acc68
Your Name <you@example.com>
2020-09-01 13:00:45 +0200

    Add feature to approve comments

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,10 @@ class Post(models.Model):
         self.save()
 
 
+    def approved_comments(self):
+        return self.comments.filter(approved=True)
+
+
     def __str__(self):
         return self.title
 
@@ -27,6 +31,12 @@ class Comment(models.Model):
     author = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
+    approved = models.BooleanField(default=False)
+
+
+    def approve(self):
+        self.approved = True
+        self.save()
 
     def __str__(self):
         return self.text
